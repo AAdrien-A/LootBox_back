1316213a87f38bedc78c15b8e8ca3297bfc7f918
LordXGhost <adrien.anodeau@gmail.com>
2020-06-12 09:52:53 +0200

    Adding route update user info

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,29 @@ router.get('/login', passport.authenticate('jwt', { session : false }), (req, re
     })
 });
 
+//Authentification + registration
 router.post('/', userCtrl.register);
 router.post('/login', userCtrl.login);
 
 // Updating One
-
+router.put('/users/:id', (req, res, next, err) => {
+    const user = new User({
+        _id: req.params.id,
+        img: req.body.img,
+        firstname: req.body.firstname,
+        surname: req.body.surname,
+        birthdate: req.body.birthdate,
+        bio: req.body.bio,
+        address: req.body.address,
+        city: req.body.city,
+        country: req.body.country,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+    });
+    User.updateOne({_id: req.params.id}, user).then(() => {
+        if (err) return res.json(err);
+        res.json(User);
+    });
+});
 
 module.exports = router;
